58be14c5416758e06fdd27f7d2fa5c4c7bbf7931
Roger Florat Gutierrez <rflorat07@gmail.com>
2023-11-04 18:59:39 +0100

    Animations Usin Rive

--- lib/features/intro/presentation/pages/loading.page.dart
+++ lib/features/intro/presentation/pages/loading.page.dart
@@ -1,17 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rive/rive.dart' as rive;
 
-class LoadingPage extends StatelessWidget {
+import '../../../../styles/styles.dart';
+
+class LoadingPage extends ConsumerStatefulWidget {
   static const String route = '/loading';
   const LoadingPage({super.key});
 
   @override
+  ConsumerState<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends ConsumerState<LoadingPage> {
+  late rive.StateMachineController smController;
+  late rive.RiveAnimation animation;
+  Map<Brightness, rive.SMIBool> states = {};
+  bool isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animation = rive.RiveAnimation.asset(
+      './assets/anims/flicky.riv',
+      artboard: 'flickylogo',
+      fit: BoxFit.contain,
+      onInit: onRiveInit,
+    );
+  }
+
+  @override
+  void dispose() {
+    smController.dispose();
+    super.dispose();
+  }
+
+  void onRiveInit(rive.Artboard artboard) {
+    smController =
+        rive.StateMachineController.fromArtboard(artboard, 'flickylogo')!;
+
+    artboard.addController(smController);
+
+    for (var theme in Brightness.values) {
+      states[theme] = smController.findInput<bool>(theme.name) as rive.SMIBool;
+      states[theme]!.value = false;
+    }
+
+    setState(() {
+      states[MediaQuery.platformBrightnessOf(context)]!.value = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = MediaQuery.platformBrightnessOf(context);
+
+    if (isInitialized) {
+      for (var valueThemes in Brightness.values) {
+        states[valueThemes]!.value = theme == valueThemes;
+      }
+    }
+
     return Scaffold(
       body: Center(
-        child: Text(
-          'Loading Page',
-          style: TextStyle(color: Theme.of(context).colorScheme.primary),
-        ),
+        child: SizedBox(
+            width: HomeAutomationStyles.loadingIconSize,
+            height: HomeAutomationStyles.loadingIconSize,
+            child: animation),
       ),
     );
   }
--- lib/features/intro/presentation/pages/splash.page.dart
+++ lib/features/intro/presentation/pages/splash.page.dart
@@ -1,25 +1,67 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rive/rive.dart' as rive;
 
+import '../../../../helpers/utils.dart';
 import 'loading.page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   static const String route = '/splash';
   const SplashPage({super.key});
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  late rive.StateMachineController smController;
+  late rive.RiveAnimation animation;
+  late rive.SMIBool state;
+  Timer splashTimer = Timer(Duration.zero, () {});
+  Timer durationTimer = Timer(Duration.zero, () {});
+
+  @override
+  void initState() {
+    super.initState();
+    animation = rive.RiveAnimation.asset(
+      './assets/anims/flicky.riv',
+      artboard: 'boltanim',
+      fit: BoxFit.fitHeight,
+      onInit: onRiveInit,
+    );
+  }
+
+  @override
+  void dispose() {
+    durationTimer.cancel();
+    splashTimer.cancel();
+    super.dispose();
+  }
+
+  void onRiveInit(rive.Artboard artboard) {
+    smController = rive.StateMachineController.fromArtboard(
+      artboard,
+      'boltanim${Theme.of(context).brightness.name}',
+    )!;
+
+    artboard.addController(smController);
+    state = smController.findInput<bool>('loaded') as rive.SMIBool;
+
+    splashTimer = Timer(const Duration(seconds: 2), () {
+      state.value = true;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () {
-      GoRouter.of(context).go(LoadingPage.route);
+    durationTimer = Timer(const Duration(seconds: 4), () {
+      GoRouter.of(Utils.mainNav.currentContext!).go(LoadingPage.route);
     });
 
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Splash Page',
-          style: TextStyle(color: Theme.of(context).colorScheme.primary),
-        ),
-      ),
+      body: animation,
     );
   }
 }
